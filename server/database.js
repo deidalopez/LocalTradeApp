4fe81dcf98f0936400d66d780829b0d772e9679e
Deida <deidalopez1@gmail.com>
2021-03-03 14:03:23 -0800

    Connected to database

--- server/database.js
+++ server/database.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose');
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_NAME = process.DB_NAME || 'localtradeDB';
+
+mongoose.connect(
+  `mongodb://localhost:${DB_PORT}/${DB_NAME}`,
+  {useNewUrlParser:true, useUnifiedTopology:true},
+
+  (error) => {
+    if (error) {
+      console.log(`error: ${error}`);
+    } else {
+      console.log( `Database connected on mongodb://localhost:${DB_PORT}/${DB_NAME}`)
+    }
+  }
+)
+
+module.exports = mongoose;

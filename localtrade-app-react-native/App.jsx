6f517b2523ad9c01b6646b31a29c2acfa9d4bb3e
deidalopez <deidalopez1@gmail.com>
2021-04-26 15:41:14 -0700

    deleting comments

--- localtrade-app-react-native/App.jsx
+++ localtrade-app-react-native/App.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import LandingPage from './components/Landing/LandingPage'
-// import Register from './components/Register/Register';
-// import Login from './components/Login/Login';
-// import Feed from './components/Feed/Feed';
-// import Dashboard from './components/Dashboard/Dashboard';
-// import Post from './components/Post/Post';
-// import MyProfile from './components/MyProfile/MyProfile';
 import { Provider } from './context/Context'
 import RootNavigator from './RootNavigator';
 import { Provider as PaperProvider } from 'react-native-paper';
